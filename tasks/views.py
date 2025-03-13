1f891850cb005c8a562ea9e8f3af9960a25063b3
emoncse2020 <emon.cse.2020@gmail.com>
2025-03-13 14:12:30 +0600

    porject setup done

---
+++ tasks/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from tasks.forms import TaskForm, TaskModelForm
+from tasks.models import Employee, Task
+
+# Create your views here.
+
+
+def manager_dashboard(request):
+    return render(request, "dashboard/manager-dashboard.html")
+
+
+def user_dashboard(request):
+    return render(request, "dashboard/user-dashboard.html")
+
+
+def test(request):
+    names = ["Mahmud", "Ahamed", "John", "Mr. X"]
+    count = 0
+    for name in names:
+        count += 1
+    context = {
+        "names": names,
+        "age": 23,
+        "count": count
+    }
+    return render(request, 'test.html', context)
+
+
+def create_task(request):
+    # employees = Employee.objects.all()
+    form = TaskModelForm()  # For GET
+
+    if request.method == "POST":
+        form = TaskModelForm(request.POST)
+        if form.is_valid():
+
+            """ For Model Form Data """
+            form.save()
+
+            return render(request, 'task_form.html', {"form": form, "message": "task added successfully"})
+
+           
+
+    context = {"form": form}
+    return render(request, "task_form.html", context)
