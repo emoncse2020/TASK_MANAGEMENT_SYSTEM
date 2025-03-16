51c69fabc0c268095eff3b312deff6ec29426bfc
emoncse2020 <emon.cse.2020@gmail.com>
2025-03-16 16:16:08 +0600

    env variable added

--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save, m2m_changed, post_delete
+from django.core.mail import send_mail
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -37,7 +40,7 @@ class TaskDetail(models.Model):
         (MEDIUM, "Medium"),
         (LOW, "Low"),
     )
-    task = models.OneToOneField(Task, on_delete= models.CASCADE, related_name='details')
+    task = models.OneToOneField(Task, on_delete= models.DO_NOTHING, related_name='details')
     # assigned_to = models.CharField(max_length=100)
     priority = models.CharField(max_length=1, choices=PRIORITY_OPTIONS)
     notes = models.TextField(blank= True, null= True)
@@ -52,3 +55,30 @@ class Project(models.Model):
 
     def __str__(self):
         return f"{self.name} | {self.start_date}"
+    
+
+#signals
+@receiver(m2m_changed, sender=Task.assigned_to.through)
+def notify_employees_on_task_creation(sender, instance, action, **kwargs):
+    if action == 'post_add':
+        print(instance, instance.assigned_to.all())
+
+        assigned_emails = [emp.email for emp in instance.assigned_to.all()]
+        print("Checking....", assigned_emails)
+
+        send_mail(
+            "New Task Assigned",
+            f"You have been assigned to the task: {instance.title}",
+            "mrhstu01@gmail.com",
+            assigned_emails,
+            fail_silently=False,
+        )
+
+
+@receiver(post_delete, sender=Task)
+def delete_associate_details(sender, instance, **kwargs):
+    if instance.details:
+        print(isinstance)
+        instance.details.delete()
+
+        print("Deleted successfully")
